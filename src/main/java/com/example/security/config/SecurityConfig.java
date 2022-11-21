93ac40f1ad7e1e82f3a826cde73e7d0f36616c2c
KimNakgyeom <himina1227@gmail.com>
2022-11-21 16:23:17 +0900

    - 인증 API: 세션 고정 보호 기능

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -63,9 +64,11 @@ public class SecurityConfig {
 
         http
                 .sessionManagement()
+                .sessionFixation().changeSessionId()   // changeSessionId 기본값 -> servlet 3.1 이상 기본 값, migrateSession -> servlet 3.1미만   none 세션 고정 보호 안됨 -> 세션 고정보호 기능
                 .maximumSessions(1) // 최대 허용 가능 세션 수, -1:무제한 로그인 세션 허용
                 .maxSessionsPreventsLogin(true) // 동시 로그인 차단함, false : 기존 세션 만료(default) -> 이전 사용자 세션 만료
                 .expiredUrl("/expired");    // 세션이 만료된 경우 이동 할 페이지
+
         return http.build();
     }
 
