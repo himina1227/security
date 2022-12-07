6645866d9d90caa6ba99858034f57cf3f2455d97
KimNakgyeom <himina1227@gmail.com>
2022-12-07 10:28:42 +0900

    - Controller Path 추가

--- src/main/java/com/example/security/domain/admin/ConfigController.java
+++ src/main/java/com/example/security/domain/admin/ConfigController.java
@@ -1,4 +1,13 @@
 package com.example.security.domain.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ConfigController {
+
+    @GetMapping("/config")
+    public String config() {
+        return "admin/config";
+    }
 }
